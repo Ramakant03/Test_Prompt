974980b15bd760481f7db97ed0400d4344ae601c
Ramakant03 <ramakantgupta1573@gmail.com>
2026-02-25 22:02:44 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+for (let i = 0; i < 5; i++) {
+    console.log("in a loop");
+}
+console.log("out of loop");
+console.log("end");
---
+++ leap_Year.js
@@ -0,0 +1,7 @@
+let Year = 1996;
+if (Year % 4 == 0) {
+    console.log("This is a leap year.");
+}
+else {
+    console.log("This is not a leap year.");
+}
